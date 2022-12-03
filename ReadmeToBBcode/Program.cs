fbad7da5860b3278ad7eb2c528d5261725ac1bde
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-12-02 21:35:54 -0500

    Ensures lines before and after header.
    This is similar to how mark down renders by default.

--- ReadmeToBBcode/Program.cs
+++ ReadmeToBBcode/Program.cs
@@ -124,9 +124,17 @@ namespace ReadmeToBBcode
 
             string outText = source.Replace("\r\n", "\n");
 
+            //Add a Line Feed before any header line if there is not one.
+            outText = Regex.Replace(outText, @"[^\n](\n#+.+?\n)", "\n$1", RegexOptions.Multiline);
+
+
+            ////Add a Line Feed after any header line if there is not one.
+            outText = Regex.Replace(outText, @"(\n#+.+\n)(?!\n)", "$1\n", RegexOptions.Multiline);
+
             outText = Regex.Replace(outText, @"^# (.+?)$", "[size=4][b]$1[/b][/size]", RegexOptions.Multiline);
             outText = Regex.Replace(outText, @"^## (.+?)$", "[size=3][b]$1[/b][/size]", RegexOptions.Multiline);
             outText = Regex.Replace(outText, @"^### (.+?)$", "[size=2][b]$1[/b][/size]", RegexOptions.Multiline);
+
 
             //Code
             outText = Regex.Replace(outText, "```(.+?)```", "[code]$1[/code]", RegexOptions.Singleline);
